8529c8e6e501d77ce4666347c6c6f51b67a54a91
aggarrohit <32900818+aggarrohit@users.noreply.github.com>
2023-04-08 21:31:21 +0200

    deploy 5

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ function Header() {
       <header className="App-header">
         <img src={require("../assets/images/video-camera.png")} className="App-logo" alt="logo" />
         <div className='title-box'>
-            <span className='title-text'>Movies Stack</span>
+            <span className='title-text'>Movie Stack</span>
             <br></br>
             <span className='subtitle-text'>Search movies, add to favorites and watch later</span>       
         </div>
